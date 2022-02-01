ff88dd5541fdd8cc10f7b35afdce449b9f4981cb
angelinaturchyn <angelinaturchyn@gmail.com>
2022-01-31 22:37:09 -0600

    add first tests add mocha config

--- specs/auth.spec.js
+++ specs/auth.spec.js
@@ -1,9 +1,24 @@
 import supertest from 'supertest'
+import {expect} from 'chai'
+
 describe('Auth',function (){
     it('Successful log in', function() {
      const request = supertest('https://paysis.herokuapp.com')
         request.post('/auth')
                .send({login:'adminius',password: 'supers3cret'})
-               .expect(200)
+               .end(function (err,res) {
+                   expect(res.statusCode).to.eq(200)
+                   expect(res.body.token).not.be.undefined
+               })
+    })
+
+    it.only('Log in with invalid credentials', function() {
+        const request = supertest('https://paysis.herokuapp.com')
+        request.post('/auth')
+            .send({login:'invalid',password: 'invalid'})
+            .end(function (err,res) {
+                expect(res.statusCode).to.eq(404)
+                expect(res.body.token).to.eq('Wrong login or password.')
+            })
     })
 })
