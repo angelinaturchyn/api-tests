1ed0206c1a0927c544c20c462fefb1d8ab6cd1ec
angelinaturchyn <angelinaturchyn@gmail.com>
2022-01-31 22:23:43 -0600

    add first tests add mocha config

--- specs/auth.spec.js
+++ specs/auth.spec.js
@@ -2,6 +2,8 @@ import supertest from 'supertest'
 describe('Auth',function (){
     it('Successful log in', function() {
      const request = supertest('https://paysis.herokuapp.com')
-        request.post('')
+        request.post('/auth')
+               .send({login:'adminius',password: 'supers3cret'})
+               .expect(200)
     })
 })
