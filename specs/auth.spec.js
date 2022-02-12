a084eba30147583648dae75f54c164eed79f3c52
angelinaturchyn <75140309+angelinaturchyn@users.noreply.github.com>
2022-02-11 22:44:49 -0600

    Update auth.spec.js

--- specs/auth.spec.js
+++ specs/auth.spec.js
@@ -16,32 +16,26 @@ describe('Auth',function (){
                 })
         it('Response body contains token', function() {
                    expect(authHelper.response.body.token).not.to.be.undefined
-
-
-describe('Auth',function (){
-    const request = supertest(process.env.BASE_URL)
-
-  import {expect} from 'chai'
-import AuthHelper from "../helpers/auth.helper";
-
-describe('Auth',function (){
-    let authHelper = new AuthHelper()
-
-
-    describe('Successful log in', function() {
-        before(async function() {
-           await authHelper.login(process.env.LOGIN, process.env.PASSWORD)
-        })
-
-        it('Response status code is 200', function() {
-                    expect(authHelper.response.statusCode).to.eq(200)
-
-                })
-        it('Response body contains token', function() {
-                   expect(authHelper.response.body.token).not.to.be.undefined
         })
 
           })
+
+    describe('Log in with invalid credentials',function () {
+
+        before(async function() {
+            await authHelper.login('fake', 'fake')
+        })
+
+        it('Response status code is 404', function () {
+            expect(authHelper.response.statusCode).to.eq(404)
+                })
+
+        it('response body contains error message', function() {
+            expect(authHelper.response.body.message).to.eq('Wrong login or password.')
+        })
+    })
+})
+
 
     describe('Log in with invalid credentials',function () {
 
