0e491a723c9b5a702da351433c7673bae27d94ee
angelinaturchyn <75140309+angelinaturchyn@users.noreply.github.com>
2022-02-18 22:08:40 -0600

    add auth helper (#8)

--- specs/auth.spec.js
+++ specs/auth.spec.js
@@ -7,18 +7,18 @@ describe('Auth',function (){
 
     describe('Successful log in', function() {
         before(async function() {
-           await authHelper.login(process.env.LOGIN, process.env.PASSWORD)
+            await authHelper.login(process.env.LOGIN, process.env.PASSWORD)
         })
 
         it('Response status code is 200', function() {
-                    expect(authHelper.response.statusCode).to.eq(200)
+            expect(authHelper.response.statusCode).to.eq(200)
 
-                })
+        })
         it('Response body contains token', function() {
-                   expect(authHelper.response.body.token).not.to.be.undefined
+            expect(authHelper.response.body.token).not.to.be.undefined
         })
 
-          })
+    })
 
     describe('Log in with invalid credentials',function () {
 
@@ -28,7 +28,7 @@ describe('Auth',function (){
 
         it('Response status code is 404', function () {
             expect(authHelper.response.statusCode).to.eq(404)
-                })
+        })
 
         it('response body contains error message', function() {
             expect(authHelper.response.body.message).to.eq('Wrong login or password.')
